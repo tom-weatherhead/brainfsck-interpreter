d3c370d14b4ea6aa08e7e6230fcb8e915b150f49
Tom Weatherhead <tomw@2hrd4u.org>
2022-08-15 12:01:53 -0600

    Dev

--- src/c/brainfsck.c
+++ src/c/brainfsck.c
@@ -18,33 +18,14 @@ import { argv, exit, stderr, stdin, stdout } from 'process';
 #include <ctype.h>
 #include <assert.h>
 
+#if !defined(BOOL) && !defined(FALSE) && !defined(TRUE)
+#define BOOL int
+#define FALSE 0
+#define TRUE 1
+#endif
+
 /*
 const characterEncoding = 'utf8';
-
-function calculateSquareBracketPairIndices(programTape: string): [number, number][] {
-	const result: [number, number][] = [];
-	const leftSquareBracketIndices: number[] = [];
-
-	programTape.split('').forEach((char: string, i: number) => {
-		if (char === '[') {
-			leftSquareBracketIndices.push(i);
-		} else if (char === ']') {
-			const j = leftSquareBracketIndices.pop();
-
-			if (typeof j === 'undefined') {
-				throw new Error('] with no matching [');
-			}
-
-			result.push([j, i]);
-		}
-	});
-
-	if (leftSquareBracketIndices.length > 0) {
-		throw new Error('[ with no matching ]');
-	}
-
-	return result;
-}
 */
 
 const int maxProgramTapeLength = 4096;
@@ -52,13 +33,16 @@ const int maxProgramTapeLength = 4096;
 char * programTape = NULL;
 // Assume that the program on the tape is terminated by a null character.
 
+const int maxDataTapeLength = 4096;
+char * dataTape = NULL;
+
 const int maxNumSquareBracketPairIndices = 20;
 int numSquareBracketPairIndices = 0;
 int squareBracketPairIndices[2 * maxNumSquareBracketPairIndices];
 
 int leftSquareBracketIndices[maxNumSquareBracketPairIndices];
 
-void calculateSquareBracketPairIndices(void) {
+BOOL calculateSquareBracketPairIndices(void) {
 	int i = 0;
 	int sp = 0;	/* Stack pointer; for the leftSquareBracketIndices */
 
@@ -70,48 +54,51 @@ void calculateSquareBracketPairIndices(void) {
 
 			if (sp >= maxNumSquareBracketPairIndices) {
 				fprintf(stderr, "LeftSquareBracketIndices stack overflow.\n");
-			} else {
-				leftSquareBracketIndices[sp++] = i;
+				return FALSE;
 			}
+
+			leftSquareBracketIndices[sp++] = i;
 		} else if (programTape[i] == ']') {
 
 			if (numSquareBracketPairIndices >= maxNumSquareBracketPairIndices) {
 				fprintf(stderr, "SquareBracketPairIndices list overflow.\n");
+				return FALSE;
 			} else if (sp == 0) {
 				fprintf(stderr, "LeftSquareBracketIndices stack underflow; unmatched ].\n");
-			} else {
-				squareBracketPairIndices[2 * numSquareBracketPairIndices] = leftSquareBracketIndices[--sp];
-				squareBracketPairIndices[2 * numSquareBracketPairIndices + 1] = i;
-				printf("SquareBracketPair %d: (%d, %d)\n", numSquareBracketPairIndices, squareBracketPairIndices[2 * numSquareBracketPairIndices], squareBracketPairIndices[2 * numSquareBracketPairIndices + 1]);
-				++numSquareBracketPairIndices;
+				return FALSE;
 			}
+
+			squareBracketPairIndices[2 * numSquareBracketPairIndices] = leftSquareBracketIndices[--sp];
+			squareBracketPairIndices[2 * numSquareBracketPairIndices + 1] = i;
+			/* printf("SquareBracketPair %d: (%d, %d)\n", numSquareBracketPairIndices, squareBracketPairIndices[2 * numSquareBracketPairIndices], squareBracketPairIndices[2 * numSquareBracketPairIndices + 1]); */
+			++numSquareBracketPairIndices;
 		}
 
 		++i;
 	}
 
 	if (sp != 0) {
-		fprintf(stderr, "Unmatched ] remaining at end of program.\n");
+		fprintf(stderr, "Unmatched [ remaining at end of program.\n");
+		return FALSE;
 	}
+
+	return TRUE;
+}
+
+int getMatchingBracketIndex(int i, int bracketIndex) {
+	int j;
+
+	for (j = 0; j < numSquareBracketPairIndices; ++j) {
+
+		if (squareBracketPairIndices[2 * j + bracketIndex] == i) {
+			return squareBracketPairIndices[2 * j + 1 - bracketIndex];
+		}
+	}
+
+	return -1; /* I.e. No match found */
 }
 
 /*
-function getMatchingBracketIndex(
-	squareBracketPairIndices: [number, number][],
-	i: number,
-	bracketIndex: number
-): number {
-	const foo = squareBracketPairIndices.find(
-		(obj: [number, number]) => obj[bracketIndex] === i - 1
-	);
-
-	if (typeof foo === 'undefined') {
-		throw new Error('getMatchingBracketIndex() : find() failed.');
-	}
-
-	return foo[1 - bracketIndex] + 1;
-}
-
 // From https://stackoverflow.com/questions/5006821/nodejs-how-to-read-keystrokes-from-stdin :
 
 async function getKeypress(): Promise<string> {
@@ -134,103 +121,6 @@ async function getKeypress(): Promise<string> {
 	});
 }
 
-async function main(): Promise<void> {
-	if (argv.length !== 3) {
-		throw new Error('argv.length !== 3');
-	}
-
-	const filepath = argv[2];
-
-	let programTapeIndex = 0;
-	let programTape = '';
-
-	try {
-		programTape = await readFile(filepath, characterEncoding);
-	} catch (error) {
-		stderr.write(`readFile() error: [${typeof error}] ${error}\n`);
-		throw error;
-	}
-
-	const squareBracketPairIndices = calculateSquareBracketPairIndices(programTape);
-
-	const dataTape = [0];
-	let dataTapeIndex = 0;
-
-	let maxCharsToPrint = -1; // Set to a negative number to avoid limiting printed chars
-	let c: string;
-
-	while (programTapeIndex < programTape.length && maxCharsToPrint !== 0) {
-		const char = programTape[programTapeIndex++];
-
-		switch (char) {
-			case '<': // Move the pointer to the left
-				if (--dataTapeIndex < 0) {
-					dataTape.unshift(0);
-					dataTapeIndex = 0;
-				}
-
-				break;
-
-			case '>': // Move the pointer to the right
-				if (++dataTapeIndex >= dataTape.length) {
-					dataTape.push(0);
-				}
-
-				break;
-
-			case '+': // Increment the memory cell at the pointer
-				dataTape[dataTapeIndex]++;
-				break;
-
-			case '-': // Decrement the memory cell at the pointer
-				dataTape[dataTapeIndex]--;
-				break;
-
-			case ',': // Input a character and store it in the cell at the pointer
-				// ? See https://stackoverflow.com/questions/3430939/node-js-readsync-from-stdin
-				dataTape[dataTapeIndex] = (await getKeypress()).charCodeAt(0);
-				break;
-
-			case '.': // Output the character signified by the cell at the pointer
-				c = String.fromCharCode(dataTape[dataTapeIndex]);
-				stdout.write(c);
-
-				if (maxCharsToPrint > 0) {
-					maxCharsToPrint--;
-				}
-
-				break;
-
-			case '[': // Jump past the matching ] if the cell at the pointer is 0
-				if (dataTape[dataTapeIndex] === 0) {
-					// Find the index of the right bracket that matches this left bracket:
-					programTapeIndex = getMatchingBracketIndex(
-						squareBracketPairIndices,
-						programTapeIndex,
-						0
-					);
-				}
-
-				break;
-
-			case ']': // Jump back to the matching [ if the cell at the pointer is nonzero
-				if (dataTape[dataTapeIndex] !== 0) {
-					// Find the index of the left bracket that matches this right bracket:
-					programTapeIndex = getMatchingBracketIndex(
-						squareBracketPairIndices,
-						programTapeIndex,
-						1
-					);
-				}
-
-				break;
-
-			default:
-				break;
-		}
-	}
-}
-
 main()
 	.then()
 	.catch((error: unknown) => {
@@ -245,13 +135,124 @@ main()
 /* int main(int argc, char * argv[]) { */
 int main(void) {
 	programTape = (char *)malloc(maxProgramTapeLength * sizeof(char));
+	dataTape = (char *)malloc(maxDataTapeLength * sizeof(char));
 
 	/* The Hello, World program: */
 	strcpy(programTape, "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
 
-	calculateSquareBracketPairIndices();
+	if (calculateSquareBracketPairIndices()) {
+		printf("Starting the Brainfsck interpreter...\n\n");
 
+		/* printf("14 -> %d\n", getMatchingBracketIndex(14, 0));
+		printf("33 -> %d\n", getMatchingBracketIndex(33, 1));
+		printf("43 -> %d\n", getMatchingBracketIndex(43, 0));
+		printf("45 -> %d\n", getMatchingBracketIndex(45, 1));
+		printf("8 -> %d\n", getMatchingBracketIndex(8, 0));
+		printf("48 -> %d\n", getMatchingBracketIndex(48, 1));
+		printf("17 -> %d\n", getMatchingBracketIndex(17, 0));
+		printf("17 -> %d\n", getMatchingBracketIndex(17, 1)); */
+
+		int programTapeIndex = 0;
+		int dataTapeIndex = 0;
+
+		memset(dataTape, 0, maxDataTapeLength * sizeof(char));
+
+		// let maxCharsToPrint = -1; // Set to a negative number to avoid limiting printed chars
+		// let c: string;
+
+		// while (programTapeIndex < programTape.length && maxCharsToPrint !== 0) {
+		while (programTapeIndex < maxProgramTapeLength && programTape[programTapeIndex] != '\0') {
+			const char ch = programTape[programTapeIndex++];
+
+			switch (ch) {
+				case '<': /* Move the pointer to the left */
+					if (--dataTapeIndex < 0) {
+						/* dataTape.unshift(0);
+						dataTapeIndex = 0; */
+						fprintf(stderr, "< : dataTapeIndex < 0\n");
+						exit(1);
+					}
+
+					break;
+
+				case '>': /* Move the pointer to the right */
+					if (++dataTapeIndex >= maxDataTapeLength) {
+						fprintf(stderr, "> : dataTapeIndex >= maxDataTapeLength\n");
+						exit(1);
+					}
+
+					break;
+
+				case '+': /* Increment the memory cell at the pointer */
+					dataTape[dataTapeIndex]++;
+					break;
+
+				case '-': /* Decrement the memory cell at the pointer */
+					dataTape[dataTapeIndex]--;
+					break;
+
+				case ',': /* Input a character and store it in the cell at the pointer */
+					/* dataTape[dataTapeIndex] = (await getKeypress()).charCodeAt(0); */
+					break;
+
+				case '.': /* Output the character signified by the cell at the pointer */
+					/* c = String.fromCharCode(dataTape[dataTapeIndex]);
+					stdout.write(c);
+
+					if (maxCharsToPrint > 0) {
+						maxCharsToPrint--;
+					} */
+
+					printf("%c", (char)dataTape[dataTapeIndex]);
+
+					break;
+
+				case '[': /* Jump past the matching ] if the cell at the pointer is 0 */
+					if (dataTape[dataTapeIndex] == 0) {
+						/* Find the index of the right bracket that matches this left bracket: */
+						programTapeIndex = getMatchingBracketIndex(
+							programTapeIndex,
+							0
+						);
+
+						if (programTapeIndex < 0) {
+							fprintf(stderr, "[ : Bad MatchingBracketIndex\n");
+							exit(1);
+						}
+					}
+
+					break;
+
+				case ']': /* Jump back to the matching [ if the cell at the pointer is nonzero */
+					/* printf("] : programTapeIndex is %d; data at %d is %d\n", programTapeIndex, dataTapeIndex, (int)dataTape[dataTapeIndex]); */
+
+					if (dataTape[dataTapeIndex] != 0) {
+						/* Find the index of the left bracket that matches this right bracket: */
+						programTapeIndex = getMatchingBracketIndex(
+							programTapeIndex - 1,
+							1
+						);
+
+						if (programTapeIndex < 0) {
+							fprintf(stderr, "] : Bad MatchingBracketIndex\n");
+							exit(1);
+						}
+					}
+
+					break;
+
+				default:
+					break;
+			}
+		}
+	} else {
+		printf("GTFO...\n");
+	}
+
+	free(dataTape);
 	free(programTape);
+
+	printf("\nThe program has terminated.\n\n");
 
 	return( 0 /* Or whatever is interpreted as "OK" */ );
 } /* main() */
